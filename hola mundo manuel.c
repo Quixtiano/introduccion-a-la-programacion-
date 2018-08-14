af7cad9a26b4f54f4ff363b45cd56930d6a326d9
Quixtiano <42361655+Quixtiano@users.noreply.github.com>
2018-08-13 20:38:12 -0500

    Add files via upload

---
+++ hola mundo manuel.c
@@ -0,0 +1,16 @@
+#include <stdio.h> /*printf(const char*,...); */
+#include <stdlib.h>
+/**main.c - este es un archivo hola mundo 
+*incluido en este curso-- solo para vitar
+*la maldici\'on dek programador --. en este curso,
+*usted aprender\'a a usar correctamente muchos de
+*los elementos del lenguaje C */
+*incluido 
+int main(int argc, char *argv[])
+{
+  putchar('A');
+  putchar('\n');
+  printf("HOLA MUNDO C \n");
+  system("PAUSE");	
+  return 0;
+}
